a25d52571546f6b537337ba4722d6bf5d721f851
brunnohm2 <118144173+brunnohm2@users.noreply.github.com>
2022-12-13 07:03:59 +0900

    Modulo 4 Exercício 2

--- Modulo04/Aula2Exercicio1/main.cpp
+++ Modulo04/Aula2Exercicio1/main.cpp
@@ -1,3 +1,17 @@
+ //
+ // Curso: C++, Curso Completo
+ // Professor: Wagner Rambo
+ // Aluno: Brunno Hiroshy Murai
+ // Modulo: 4
+ // Aula: 2 Exercicio
+ // Data: 2022-12-13
+ // IDE Code::Blocks
+ // Arquivo main.cpp
+ // Desenvolva um programa em C++ para copiar o conteúdo de um array
+ //   em outro de mesmo tamanho utilizando ponteiros,
+ //   tanto para imprimir os conteúdos na tela, quanto para realizar a cópia.
+ //
+
 int main(){
     int const tamanho_array = 10;
     int array1[tamanho_array] = {5,15,44,22,99,77,65,54,63,25};
@@ -6,8 +20,6 @@ int main(){
     int *ponteiro1 = array1;
     int *ponteiro2 = array2;
 
-    ponteiro2 = ponteiro1;
-
     for(int i=0; i<tamanho_array; i++)
-        array2[i]=*(ponteiro2+i);
+        *(ponteiro2+i) = *(ponteiro1+i);
 }
